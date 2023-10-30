b9974f4409b6f4a9a65e69e33856744fb6b53ae5
Dave-Howard <fatdaveh@gmail.com>
2023-10-30 17:14:37 +0000

    Update cypress urls

--- vue_blog/cypress/e2e/spec.cy.js
+++ vue_blog/cypress/e2e/spec.cy.js
@@ -1,8 +1,8 @@
 /// <reference types="Cypress" />
 
 describe('My First Test', () => {
-  it('OPens the home page', () => {
-    cy.visit('http://127.0.0.1:3000/')
+  it('Opens the home page', () => {
+    cy.visit('http://127.0.0.1:5173/')
     cy.contains('Welcome')
     cy.contains('Home').click()
     cy.url().should('include', '/')
