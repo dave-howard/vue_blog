66da1cd21dcf3e335a29915e4eedbde5bf4c76d7
Dave-Howard <fatdaveh@gmail.com>
2023-11-11 20:37:53 +0000

    edit home view header

--- vue_blog/src/views/HomeView.vue
+++ vue_blog/src/views/HomeView.vue
@@ -7,7 +7,7 @@ if (!useBlogStore().blog_posts) useBlogStore().get_posts()
 
 <template>
   <div class="row">
-    <h1 class="text-center">Welcome to the CodyPy blog</h1>
+    <h1 class="text-center">Welcome to the CodyPy blog!</h1>
   </div>
   <template v-for="post in useBlogStore().pinned_posts" :key="post.id">
     <div class="row mb-2">
